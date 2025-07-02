3d22d84a5af2140cf70f8cd8f25757aa757229a2
bcheban <128683111+bcheban@users.noreply.github.com>
2025-07-02 18:47:12 +0300

    fixed problem with logo

--- src/components/sections/Brands.tsx
+++ src/components/sections/Brands.tsx
@@ -13,7 +13,7 @@ export const Brands = () => {
             <div className="flex justify-center flex-wrap gap-4">
                 {logos.map((logo, key) => (
                     <div key={key} className="p-4 sm:p-5 rounded-xl bg-body border border-box-border group">
-                        <img src={`src/assets/logos/${logo}.png`} width="100" height="60" alt="logo" className="h-7 sm:h-10 w-auto ease-linear duration-300 grayscale group-hover:!grayscale-0 group-hover:scale-105"/>
+                        <img src={`/assets/logos/${logo}.png`} width="100" height="60" alt="logo" className="h-7 sm:h-10 w-auto ease-linear duration-300 grayscale group-hover:!grayscale-0 group-hover:scale-105"/>
                     </div>
                 ))}
             </div>
